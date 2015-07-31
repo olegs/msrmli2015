73da05388e9bf4044a3b0067a64b410ebe613dd2
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2015-07-31 16:34:11 +0300

    Feature selection on Christine doesn't seem to work, stay with RForest

--- Petersburg_sample_code_v3/christine.py
+++ Petersburg_sample_code_v3/christine.py
@@ -1,13 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
 
 __author__ = 'oleg'
 
 def christine(D, n_estimators):
-    clf = Pipeline([
-        ('feature_selection', LinearSVC(penalty="l1", dual=False)),
-        ('classification', RandomForestClassifier(n_estimators, random_state=1))
-    ])
-    return clf.fit(D.data['X_train'], D.data['Y_train'])
-    # return RandomForestClassifier(n_estimators, random_state=1).fit(D.data['X_train'], D.data['Y_train'])
+    return RandomForestClassifier(n_estimators, random_state=1).fit(D.data['X_train'], D.data['Y_train'])
