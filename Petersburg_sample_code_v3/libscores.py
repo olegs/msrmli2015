ac3ebb5555b03702f90e88ddd51b650c3ea71819
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2015-08-03 09:25:57 +0300

    Added auc_cv metrics for round 2 optimization

--- Petersburg_sample_code_v3/libscores.py
+++ Petersburg_sample_code_v3/libscores.py
@@ -20,17 +20,13 @@
 # CONNECTION WITH THE USE OR PERFORMANCE OF SOFTWARE, DOCUMENTS, MATERIALS, 
 # PUBLICATIONS, OR INFORMATION MADE AVAILABLE FOR THE CHALLENGE. 
 
-from sys import stderr
-
 import numpy as np
 import scipy as sp
 
-swrite = stderr.write
-
 
 def binarize_predictions(array):
-    ''' Turn predictions into decisions {0,1} by selecting the class with largest 
-    score for multiclass problems and thresholding at 0.5 for other cases.'''
+    """ Turn predictions into decisions {0,1} by selecting the class with largest
+    score for multiclass problems and thresholding at 0.5 for other cases."""
     # add a very small random value as tie breaker (a bit bad because this changes the score every time)
     # so to make sure we get the same result every time, we seed it    
     # eps = 1e-15
@@ -42,8 +38,8 @@ def binarize_predictions(array):
 
 
 def acc_stat(solution, prediction):
-    ''' Return accuracy statistics TN, FP, TP, FN
-     Assumes that solution and prediction are binary 0/1 vectors.'''
+    """ Return accuracy statistics TN, FP, TP, FN
+     Assumes that solution and prediction are binary 0/1 vectors."""
     # This uses floats so the results are floats
     TN = sum(np.multiply((1 - solution), (1 - prediction)))
     FN = sum(np.multiply(solution, (1 - prediction)))
@@ -57,9 +53,9 @@ def acc_stat(solution, prediction):
 
 
 def mvmean(R, axis=0):
-    ''' Moving average to avoid rounding errors. A bit slow, but...
+    """ Moving average to avoid rounding errors. A bit slow, but...
     Computes the mean along the given axis, except if this is a vector, in which case the mean is returned.
-    Does NOT flatten.'''
+    Does NOT flatten."""
     if len(R.shape) == 0: return R
     average = lambda x: reduce(lambda i, j: (0, (j[0] / (j[0] + 1.)) * i[1] + (1. / (j[0] + 1)) * j[1]), enumerate(x))[
         1]
@@ -72,10 +68,10 @@ def mvmean(R, axis=0):
 
 
 def bac_metric(solution, prediction):
-    '''
+    """
      Here we used hacked BAC to apply to solution without any wrapping
     Compute the normalized balanced accuracy. The binarization and
-    the normalization differ for the multi-label and multi-class case. '''
+    the normalization differ for the multi-label and multi-class case. """
     bin_prediction = binarize_predictions(prediction)
     [tn, fp, tp, fn] = acc_stat(solution, bin_prediction)
     # Bounding to avoid division by 0
@@ -94,6 +90,58 @@ def bac_metric(solution, prediction):
     return score
 
 
+def tiedrank(a):
+    """ Return the ranks (with base 1) of a list resolving ties by averaging.
+     This works for numpy arrays."""
+    m = len(a)
+    # Sort a in ascending order (sa=sorted vals, i=indices)
+    i = a.argsort()
+    sa = a[i]
+    # Find unique values
+    uval = np.unique(a)
+    # Test whether there are ties
+    R = np.arange(m, dtype=float) + 1  # Ranks with base 1
+    if len(uval) != m:
+        # Average the ranks for the ties
+        oldval = sa[0]
+        newval = sa[0]
+        k0 = 0
+        for k in range(1, m):
+            newval = sa[k]
+            if newval == oldval:
+                # moving average
+                R[k0:k + 1] = R[k - 1] * (k - k0) / (k - k0 + 1) + R[k] / (k - k0 + 1)
+            else:
+                k0 = k;
+                oldval = newval
+    # Invert the index
+    S = np.empty(m)
+    S[i] = R
+    return S
+
+
+def auc_metric(solution, prediction):
+    """ Normarlized Area under ROC curve (AUC).
+    Return Gini index = 2*AUC-1 for  binary classification problems.
+    Should work for a vector of binary 0/1 (or -1/1)"solution" and any discriminant values
+    for the predictions. If solution and prediction are not vectors, the AUC
+    of the columns of the matrices are computed and averaged (with no weight).
+    The same for all classification problems (in fact it treats well only the
+    binary and multilabel classification problems)."""
+    # auc = metrics.roc_auc_score(solution, prediction, average=None)
+    # There is a bug in metrics.roc_auc_score: auc([1,0,0],[1e-10,0,0]) incorrect
+    label_num = solution.shape[1]
+    auc = np.empty(label_num)
+    for k in range(label_num):
+        r_ = tiedrank(prediction[:, k])
+        s_ = solution[:, k]
+        if sum(s_) == 0: print('WARNING: no positive class example in class {}'.format(k + 1))
+        npos = sum(s_ == 1)
+        nneg = sum(s_ < 1)
+        auc[k] = (sum(r_[s_ == 1]) - npos * (npos + 1) / 2) / (nneg * npos)
+    return 2 * mvmean(auc) - 1
+
+
 from sklearn import cross_validation
 from libscores import bac_metric
 
@@ -101,3 +149,8 @@ from libscores import bac_metric
 def bac_cv(m, x, y):
     predicted = cross_validation.cross_val_predict(m, x, y, cv=5, n_jobs=-1)
     return bac_metric(y, predicted)
+
+
+def auc_cv(m, x, y):
+    predicted = cross_validation.cross_val_predict(m, x, y, cv=5, n_jobs=-1)
+    return auc_metric(y, predicted)
